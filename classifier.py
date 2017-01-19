e80277d4b59df87c5b3c86e2a95e78a7af273ab5
asaggo <asaggo0717@gmail.com>
2017-01-19 12:44:30 -0700

    Add files via upload

--- classifier.py
+++ classifier.py
@@ -1,11 +1,80 @@
+
 class Classifier(object):
-    def train(self,data_train,target_train):
-        pass
-    def predict(self,data_test):
-        list = []
-        for row in data_test:
-            result = self.classify(row)
-            list.append(result)
-        return list
+    def __init__(self):
+        self.distances = []
+        self.percent = 0.0
+        self.results = []
+
+    def train(self,data_train,target_train,target_test):
+        self.data_train = data_train
+        self.target_train = target_train
+        self.target_test = target_test
+
+    def mostCommon(self,threeIndex):
+        targetList = []
+        for i in threeIndex:
+            targetList.append(self.target_train[i])
+
+        check = [0, 0, 0]
+        for i in targetList:
+            check[i] += 1
+
+        mostCommonTarget = check.index(max(check))
+        return mostCommonTarget
+
+
+
+    def predict(self,data_test,k):
+        # list = []
+        # for row in data_test:
+        #     result = self.classify(row)
+        #     list.append(result)
+        # return list
+
+        for item in data_test: #send each row in data_test
+            self.distances.append(self.getDistance(item, k)) # k == 3, the number of neighbor
+
+        #print (self.distances)
+
+
+        for each in self.distances:
+            threeIndex = []
+            for i in each:
+                threeIndex.append(i[1])
+            self.results.append(self.mostCommon(threeIndex))
+
+        #After this, need tocompare results and target_test
+
+    def compare(self):
+        count = 0
+        for i in range(len(self.results)):
+            if (self.results[i] == self.target_test[i]):
+                count += 1
+
+        self.percent = round(100*(count / float(len(self.results))),2)
+        print "Accuracy: " + str(self.percent) + "%"
+
+    def getDistance(self,item,k):
+        distance = []
+        for row in range(len(self.data_train)): #all integer
+            rowDistance = 0
+            for col in range(len(self.data_train[row])):
+                rowDistance += ((item[col]-self.data_train[row][col])**2)
+            distance.append((rowDistance,row))
+        distance.sort()
+        return distance[:k]
+        #self.distances.sort()
+
+        #return self.distances
+
+
+
+
+
+
     def classify(self,row):
         return 0
+
+
+
+
--- prove01.py
+++ prove01.py
@@ -2,22 +2,32 @@ from sklearn import datasets
 from sklearn.cross_validation import train_test_split as tts
 import classifier
 import random
+
+from copy import copy, deepcopy
+
+
 iris = datasets.load_iris()
 data_train, data_test, target_train, target_test = tts(iris.data, iris.target, train_size=.7, random_state=random.randint(100,500))
-
-def compare(list, target_test):
-    count = 0
-
-    for i in range(len(target_test)):
-        if (list[i] == target_test[i]):
-            count += 1
-    return count
+#
+# def compare(list, target_test):
+#     count = 0
+#
+#     for i in range(len(target_test)):
+#         if (list[i] == target_test[i]):
+#             count += 1
+#     return count
 
 cf = classifier.Classifier()
-cf.train(data_train,target_train)
-list = cf.predict(data_test)
-count = compare(list,target_test)
-percent = count/float(len(target_test))
-percent = round(percent*100,2)
-print "accuracy:",percent,"%"
+cf.train(data_train,target_train,target_test)
+k = raw_input("How many neighbors? ")
+result = []
+result = cf.predict(data_test,int(k)) #has 0,0,1 or 0,1,2 or something like that
+cf.compare()
+
+
+
+#count = compare(list,target_test)
+#percent = count/float(len(target_test))
+#percent = round(percent*100,2)
+#print "accuracy:",percent,"%"
 
